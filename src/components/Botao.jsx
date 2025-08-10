0244e17e2003cf2791c6288051eba689423323f7
anamoraeszx <ana.moraes@sptech.school>
2025-08-10 15:38:56 -0300

    UPDATE nas telas de Cadastro de beneficiario e Cadastro de novas atividades | Adição de novos componentes

--- src/components/Botao.jsx
+++ src/components/Botao.jsx
@@ -1,13 +1,4 @@
-// import "../css/botao.css";
-
-// export default function Botao(props) {
-//     return (
-//         <button className="botao" type={props.type} onClick={props.onClick}>
-//             {props.children}</button>
-//     )
-// }
-
-import "../css/botao.css";
+import "../css/form-controls.css";
 
 export default function Botao(props) {
     const { type = "button", variant = "primario", className = "", onClick, children } = props;
